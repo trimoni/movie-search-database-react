172126e2034abd22d59b23307e8f3edd13687155
Tal <trimoni8@gmail.com>
2022-12-21 11:28:29 -0500

    p tag

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -5,6 +5,8 @@ function Popup({selected, closePopup}) {
     <section className='popup'>
       <div className='content'>
         <h2>{selected.Title} <span>({ selected.Year})</span></h2>
+        <p className='rating'>Rating: {selected.imdbRating}</p>
+        
       </div>
     </section>
   )
