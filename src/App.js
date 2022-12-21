82dc6d9a68d51e60366967069c0c4b649caf9ab4
Tal <trimoni8@gmail.com>
2022-12-21 11:34:25 -0500

    pass openPopup

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ function App() {
       </header>
       <main>
         <Search handleInput={handleInput} search={search}/>
-        <Results results={state.results} />
+        <Results results={state.results} openPopup={openPopup}/>
         {(typeof state.selected.Title != 'undefined') ? <Popup selected={state.selected} closePopup={closePopup}/> : false}
       </main>
     </div>
